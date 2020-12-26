0d023f197f5d319d2758b3df1719e24c0bfb631e
OMAMA TAHIR <omamagoldstar@gmail.com>
2020-12-26 15:04:15 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -21,19 +21,19 @@ function Hi() {
     <h2 className="postHeading">Facebook Post</h2>
     <div className="input-container">
     <label className="postLabel">UserName</label>
-    <input type="text" value={handle.name} onChange={e => sethandle({...handle,name:e.target.value})} className="input1"></input>
+    <input type="text" value={handle.name} onChange={e => sethandle({...handle,name:e.target.value})} className="input1" placeholder="Enter your Name"></input>
     </div>
     <div className="input-container">
     <label className="postLabel">ProfileImage</label>
-    <input type="text" value={handle.profile} onChange={e => sethandle({...handle,profile:e.target.value})} className="input2"></input>
+    <input type="text" value={handle.profile} onChange={e => sethandle({...handle,profile:e.target.value})} className="input2" placeholder="Paste profile image link here"></input>
     </div>
     <div className="input-container">
     <label className="postLabel">Text</label>
-    <input type="text" value={handle.text} onChange={e => sethandle({...handle,text:e.target.value})} className="input3"></input>
+    <input type="text" value={handle.text} onChange={e => sethandle({...handle,text:e.target.value})} className="input3" placeholder="Enter text here"></input>
     </div>
     <div className="input-container">
     <label className="postLabel">Post</label>
-    <input type="text" value={handle.img} onChange={e => sethandle({...handle,img:e.target.value})} className="input4"></input>
+    <input type="text" value={handle.img} onChange={e => sethandle({...handle,img:e.target.value})} className="input4" placeholder="Paste your post link here"></input>
     </div>
     <button  onClick={postContent} type="button" class="btn btn-danger" className="postBtn">Post</button>
     {posts.map(data=> <Facebook data={data}/>)}
